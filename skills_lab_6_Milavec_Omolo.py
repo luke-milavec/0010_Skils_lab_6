0bd2998f631b04da39f2ae5408be0e9e798cb090
luke-milavec <71520585+luke-milavec@users.noreply.github.com>
2020-10-06 03:24:30 -0400

    Update skills_lab_6_Milavec_Omolo.py

--- skills_lab_6_Milavec_Omolo.py
+++ skills_lab_6_Milavec_Omolo.py
@@ -1,4 +1,8 @@
 name = input("What is your name")
+#Takes input for name
 print("Hello "+ name)
+#greets user
 grade = input("What grade are you in?")
+#takes grade input
 print("Your name is "+ name + " and you are in grade " + grade")
+#displays final statement
